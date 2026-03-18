27ce1352b69f2f0f6043e6121f9c11ba5db165a5
konstantinshishkin <k.shishkin@brandquad.ru>
2026-03-18 18:48:33 +0300

    Remove SVG icon from photo gallery button for a cleaner design and improved readability.

--- wp-content/themes/paprika/front-page.php
+++ wp-content/themes/paprika/front-page.php
@@ -65,7 +65,7 @@ require get_theme_file_path('includes/header.php');
       <div class="card">
         <h2>Фотогалерея</h2>
         <p>Детали, подача, эмоции гостей и работа шефа. Здесь вкус выглядит так же ярко, как ощущается.</p>
-        <a class="btn" href="/fotogalereya/">Посмотреть все фото <span style="fill:white"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640"><path d="M169.4 297.4C156.9 309.9 156.9 330.2 169.4 342.7L361.4 534.7C373.9 547.2 394.2 547.2 406.7 534.7C419.2 522.2 419.2 501.9 406.7 489.4L237.3 320L406.6 150.6C419.1 138.1 419.1 117.8 406.6 105.3C394.1 92.8 373.8 92.8 361.3 105.3L169.3 297.3z"/></svg></span></a>
+        <a class="btn" href="/fotogalereya/">Посмотреть все фото</a>
       </div>
       <div class="grid">
           <?php
